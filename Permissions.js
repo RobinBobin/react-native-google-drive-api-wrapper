18e24224aaf2a3d112df2ee1abb2275903f062cf
Miodrag Peric <misha@byteout.com>
2020-07-09 09:54:10 +0200

    Test change

--- Permissions.js
+++ Permissions.js
@@ -4,6 +4,7 @@ const permissions = "/permissions";
 
 export default class Permissions {
    create(fileId, params) {
+      console.log('vvvv');
       const body = JSON.stringify(params);
       
       return fetch(`${GDrive._urlFiles}/${fileId}${permissions}`, {
