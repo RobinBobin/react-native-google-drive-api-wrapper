8637e39ee0bbb95ab6efa60bcc197160afdd3814
Miodrag Peric <misha@byteout.com>
2020-07-09 10:10:42 +0200

    ...

--- Permissions.js
+++ Permissions.js
@@ -1,12 +1,37 @@
 import GDrive from "./GDrive";
+import {
+   StaticUtils,
+   ArrayStringifier
+} from "simple-common-utils";
 
 const permissions = "/permissions";
 
+function _stringifyQueryParams(queryParams,
+   prefix = "?", separator = "&", quoteIfString)
+{
+   const array = [];
+   
+   Object.keys(queryParams).forEach(key => array.push(
+      `${key}=${StaticUtils.safeQuoteIfString(queryParams[key], quoteIfString)}`));
+   
+   return new ArrayStringifier(array)
+      .setPrefix(prefix)
+      .setSeparator(separator)
+      .process();
+}
+
 export default class Permissions {
-   create(fileId, params) {
+   create(fileId, params, queryParams) {
       const body = JSON.stringify(params);
       
-      return fetch(`${GDrive._urlFiles}/${fileId}${permissions}`, {
+      let finalQueryParams = '';
+      if (queryParams) {
+         finalQueryParams = _stringifyQueryParams(queryParams);
+      }
+
+      console.log('URL BRE', `${GDrive._urlFiles}/${fileId}${permissions}${finalQueryParams}`);
+
+      return fetch(`${GDrive._urlFiles}/${fileId}${permissions}${finalQueryParams}`, {
          method: "POST",
          headers: GDrive._createHeaders(
             GDrive._contentTypeJson,
