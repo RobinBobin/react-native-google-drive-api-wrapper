8f79fd972a373b15ca933fac78941ae49bbe7cee
mi5ha <1238203+mi5ha@users.noreply.github.com>
2020-07-13 12:10:47 +0200

    Added third argument to Permissions.create (#31)
    * Test change
    
    * Removed small change
    
    * ...
    
    * Add third arg for Permissions.create
    
    * Refactored code
    
    * Used ternary operator
    
    * Updated documentation
    
    Co-authored-by: Miodrag Peric <misha@byteout.com>

--- Permissions.js
+++ Permissions.js
@@ -3,10 +3,12 @@ import GDrive from "./GDrive";
 const permissions = "/permissions";
 
 export default class Permissions {
-   create(fileId, params) {
+   create(fileId, params, queryParams) {
       const body = JSON.stringify(params);
-      
-      return fetch(`${GDrive._urlFiles}/${fileId}${permissions}`, {
+
+      queryParams = queryParams ? GDrive._stringifyQueryParams(queryParams) : "";
+
+      return fetch(`${GDrive._urlFiles}/${fileId}${permissions}${queryParams}`, {
          method: "POST",
          headers: GDrive._createHeaders(
             GDrive._contentTypeJson,
